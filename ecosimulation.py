bede675e09902a93510a2e7372ec676de02d4586
jonas <jonas@jonasberggren1133@gmail.com>
2019-12-12 09:16:55 +0100

    kfjkldsjf

--- ecosimulation.py
+++ ecosimulation.py
@@ -597,7 +597,7 @@ while running:
                 size = (width,height)
                 img_array.append(img)
 
-            out = cv2.VideoWriter(logpath + '/video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+            out = cv2.VideoWriter(logpath + '/video.avi', cv2.cv.CV_FOURCC(*'DIVX'), 15, size)
 
             for i in range(len(img_array)):
                 out.write(img_array[i])
